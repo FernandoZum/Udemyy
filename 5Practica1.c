cd2d351214ba97726af3886816032df2b61c7352
FernandoZum <130106853+FernandoZum@users.noreply.github.com>
2023-07-19 18:05:49 -0500

    Add files via upload

---
+++ 5Practica1.c
@@ -0,0 +1,12 @@
+//Inclusion de Librerias
+#include <stdio.h>
+#include <stdlib.h>
+
+//Para que el programa se ejecute. SIEMPRE poner dentro  de "int main()"
+int main()
+{
+    //Mostrara el texto en pantalla "Printf"
+    printf("Hola mundo Lenguaje C\n");
+    //Return hara que el valor regrese a 0 despues de ejecutar el programa
+    return 0;
+}
